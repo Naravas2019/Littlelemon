4b87fcb759efc5cc947ccfb439bda9bf4eba6602
Naravas2019 <49953331+Naravas2019@users.noreply.github.com>
2023-07-04 13:57:09 +0200

    updates

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,23 +1,32 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import Booking
-from .serializers import BookingSerializer
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .models import Menu, Booking
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework import generics
 from rest_framework import viewsets
-from django.contrib.auth.models import User
-from rest_framework import viewsets, permissions
-from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
+from rest_framework.decorators import permission_classes
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-
-
+# Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'index.html', {})
 
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
 
-class BookingViewSet(ModelViewSet):
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-   queryset = User.objects.all()
-   serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated] 
